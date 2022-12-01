5b430f8e44088a2bc8724d5f2697de2dbe1eb393
cwijnekus <clemens.wijnekus@jayway.com>
2022-12-01 22:19:55 +0100

    Star 2

--- 01/main.go
+++ 01/main.go
@@ -7,8 +7,6 @@ import (
 	"strings"
 )
 
-var elves map[int]int = make(map[int]int)
-
 func main() {
 	inventories, err := ioutil.ReadFile("input") // the file is inside the local directory
 	if err != nil {
@@ -16,22 +14,32 @@ func main() {
 	}
 	splitInventories := strings.Split(string(inventories), "\n\n")
 
-	for k, v := range splitInventories {
+	var elves []int = make([]int, len(splitInventories))
+	for _, v := range splitInventories {
 		calorieStrings := strings.Split(v, "\n")
+		total := 0
 		for _, c := range calorieStrings {
 			calorie, _ := strconv.ParseInt(c, 10, 0)
-			elves[k] = elves[k] + int(calorie)
+			total += int(calorie)
+		}
+		elves = append(elves, total)
+	}
+
+	for k := range elves {
+		for l := range elves {
+			if elves[k] < elves[l] {
+				elves[k], elves[l] = elves[l], elves[k]
+			}
 		}
 	}
 
-	var elf int = 0
-	var calories int = 0
-	for k, v := range elves {
-		if v > calories {
-			calories = v
-			elf = k
-		}
+	fmt.Printf("Elf that has the biggest calorie stack has: %d ", elves[len(elves)-1])
+
+	together := 0
+	for _, v := range elves[len(elves)-3:] {
+		together += v
 	}
 
-	fmt.Printf("Elf %d has the biggest calorie stack with: %d ", elf, calories)
+	fmt.Printf("The three elves carrying the most are carrying %d together", together)
+
 }
