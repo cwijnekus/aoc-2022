6a3e148565b244e978430242906ddbe3d6ed3a69
cwijnekus <clemens.wijnekus@jayway.com>
2022-12-01 21:44:17 +0100

    Start 1

--- 01/main.go
+++ 01/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
 
+var elves map[int]int = make(map[int]int)
+
+func main() {
+	inventories, err := ioutil.ReadFile("input") // the file is inside the local directory
+	if err != nil {
+		fmt.Println("Err")
+	}
+	splitInventories := strings.Split(string(inventories), "\n\n")
+
+	for k, v := range splitInventories {
+		calorieStrings := strings.Split(v, "\n")
+		for _, c := range calorieStrings {
+			calorie, _ := strconv.ParseInt(c, 10, 0)
+			elves[k] = elves[k] + int(calorie)
+		}
+	}
+
+	var elf int = 0
+	var calories int = 0
+	for k, v := range elves {
+		if v > calories {
+			calories = v
+			elf = k
+		}
+	}
+
+	fmt.Printf("Elf %d has the biggest calorie stack with: %d ", elf, calories)
 }
