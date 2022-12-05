0932e8c1a9baab706b8986db2e4a2d0b807a7d7f
cwijnekus <clemens.wijnekus@devoteam.com>
2022-12-05 12:01:54 +0100

    02 first star

--- 02/main.go
+++ 02/main.go
@@ -13,6 +13,8 @@ var scoringMap map[string]map[string]int = map[string]map[string]int{
 	"C": {"X": 6, "Y": 0, "Z": 3},
 }
 
+var winningMap map[string]string
+
 func main() {
 	input, err := ioutil.ReadFile("input")
 	if err != nil {
@@ -21,12 +23,38 @@ func main() {
 	rounds := strings.Split(string(input), "\n")
 
 	score := 0
+	secretScore := 0
 	for _, v := range rounds {
 		plays := strings.Split(v, " ")
 		score += determineScore(plays[0], plays[1])
+		secretScore += determineOutcome(plays[0], plays[1])
 	}
 
 	fmt.Printf("Total end score is: %d", score)
+	fmt.Printf("Total end secretscore is: %d", secretScore)
+
+}
+
+func determineOutcome(play string, outcome string) int {
+	if strings.EqualFold(outcome, "Y") {
+		return playScore(play) + 3
+	} else if strings.EqualFold(outcome, "X") {
+		if strings.EqualFold(play, "A") {
+			return playScore("C")
+		} else if strings.EqualFold(play, "B") {
+			return playScore("A")
+		} else {
+			return playScore("B")
+		}
+	} else {
+		if strings.EqualFold(play, "A") {
+			return playScore("B") + 6
+		} else if strings.EqualFold(play, "B") {
+			return playScore("C") + 6
+		} else {
+			return playScore("A") + 6
+		}
+	}
 }
 
 func determineScore(play string, opposed string) int {
@@ -35,11 +63,11 @@ func determineScore(play string, opposed string) int {
 
 func playScore(play string) int {
 	switch play {
-	case "X":
+	case "A", "X":
 		return 1
-	case "Y":
+	case "B", "Y":
 		return 2
-	case "Z":
+	case "C", "Z":
 		return 3
 	default:
 		log.Panic("Could not determine score from play:", play)
